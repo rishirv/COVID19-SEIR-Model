c25103826b4cab0240f4f296eb37f655e611f2d0
Rishi Verma <verma.rishiraj@gmail.com>
2020-07-19 11:18:56 -0400

    Working sliders!

--- seir.py
+++ seir.py
@@ -34,26 +34,48 @@ S, I, R = sol.T
 #Plot the results
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.25, bottom=0.25)
-ax.plot(time, S, 'b', label = 'Susceptible')
-ax.plot(time, I, 'r', label = 'Infected')
-ax.plot(time, R, 'g', label = 'Recovered')
-ax.plot(time, S+I+R, '--', label = 'Total') #Mark total with a dashed line
+Splt, = ax.plot(time, S, 'b', label = 'Susceptible')
+Iplt, = ax.plot(time, I, 'r', label = 'Infected')
+Rplt, = ax.plot(time, R, 'g', label = 'Recovered')
+Tplt, = ax.plot(time, S+I+R, '--', label = 'Total') #Mark total with a dashed line
 ax.set(xlabel = 'Time (days)', ylabel = "People", title = 'Disease Spread')
 plt.legend()
 
 #Add sliders for input parameters
-sliderColor = 'red'
-betaAx = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor = sliderColor)
-betaSlider = Slider(betaAx, 'Beta', 1, 10, valinit = 3, valstep = 1)
+sliderColor = 'green'
+betaAx = plt.axes([0.25, 0.1, 0.65, 0.03])
+betaSlider = Slider(betaAx, 'Beta', 1, 10, valinit = 3, valstep = 1, color = sliderColor)
 
-DAx = plt.axes([0.25, 0.15, 0.65, 0.03], facecolor = sliderColor)
-gammaSlider = Slider(DAx, 'Days to recover (1/Gamma)', 1, 15, valinit = 5, valstep = 1)
+DAx = plt.axes([0.25, 0.15, 0.65, 0.03])
+DSlider = Slider(DAx, 'Days to recover (1/Gamma)', 1, 15, valinit = 5, valstep = 1, color = sliderColor)
 
 def reset(event):
-    betaSlider.reset()
-    gammaSlider.reset()
-resetAx = fig.add_axes([0.8, 0.025, 0.1, 0.04])
-resetButton = Button(resetAx, 'Reset', color = sliderColor)
+	'''Reset all of the sliders'''
+	betaSlider.reset()
+	DSlider.reset()
+resetAx = fig.add_axes([0.45, 0.025, 0.1, 0.04])
+resetButton = Button(resetAx, 'Reset', color = 'gray')
 resetButton.on_clicked(reset)
+
+def model(event):
+	'''Update the parameters and plots'''
+	beta = betaSlider.val
+	gamma = 1/DSlider.val
+
+	#Differentiate the equations again
+	sol = odeint(SEIR, initVec, time, args = (N, beta, gamma))
+	S, I, R = sol.T
+
+	#Change the y-data on each of the plots
+	Splt.set_ydata(S)
+	Iplt.set_ydata(I)
+	Rplt.set_ydata(R)
+	Tplt.set_ydata(S + I + R)
+
+	#Redraw the plots
+	fig.canvas.draw_idle()
+setAx = fig.add_axes([0.6, 0.025, 0.1, 0.04])
+setButton = Button(setAx, 'Model', color = 'gray')
+setButton.on_clicked(model)
 
 plt.show()
