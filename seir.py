ea4fb640642e2e0ea4256dbbfec097747239a541
Rishi Verma <verma.rishiraj@gmail.com>
2020-07-29 22:18:03 -0400

    Small updates to DifEQs

--- seir.py
+++ seir.py
@@ -3,46 +3,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
 
-def SEIR(currentVec, time, N, beta, gamma1, gamma2, miu, delta, alpha, rho):
+def SEIR(currentVec, time, N, beta, gamma1, gamma2, eta, mu, delta, alpha, rho):
 	'''
 	N = The total population
 	beta = Average number of people an asymptotic person infects every day
+	D = Number of days it takes for an asymptomatic person to recover (1/gamma1)
 	gamma1 = Proportion of asymptomatic people who recover each day (1/D)
 	gamma2 = Proportion of miserable people who recover each day
+	eta = Proportion of miserable people who become asymptotic each day
 	delta = Expected number of days of incubation
-	miu = proportion of asymptotic people that become miserable( = 1-gamma1)
+	mu = Proportion of asymptotic people that become miserable
 	alpha = Fatality rate = Proportion of miserable people that die 
 	rho = Proportion of miserable people who die each day (one divided by the number of days it takes for an infected person to die)
-	D = Number of days it takes for an asymptomatic person to recover (1/gamma1)
-	R0 = Total number of people an asymptomatic person will infect (beta/gamma) 
+	R0 = Total number of people an asymptomatic person will infect (beta/gamma1) 
 	IN THIS MODEL WE ASSUME MISERABLE PPL DONT INFECT ANYONE
 	'''
 	S, E, A, M, R, D = currentVec
 
-	dS = -beta*E*S/(N-D)
-	dE = beta*E*S/(N-D) - delta*E
-	dA = delta*E -gamma1*A-miu*A
-	dM = miu*A-gamma2*(1-alpha)*M - rho*alpha*M
-	dR = (gamma2)*(1-alpha)*M+gamma1*A
+	dS = -beta*A*S/(N - D)
+	dE = beta*A*S/(N - D) - delta*E
+	dA = delta*E - gamma1*(1-mu)*A - mu*eta*A
+	dM = mu*eta*A - gamma2*(1-alpha)*M - rho*alpha*M
+	dR = gamma2*(1-alpha)*M + gamma1*(1-mu)*A
 	dD = rho*alpha*M
 
 	return [dS, dE, dA, dM, dR, dD]
 
-time = np.linspace(0, 49, 100) #Equally spaced time values in the range
+time = np.linspace(0, 49, 150) #Equally spaced time values in the range
 
 #Initialize the parameters
 N = 10000
-beta = 4
-gamma1 = gamma2 = 1/12
-miu = 1/2
+beta = 1
+gamma1 = gamma2 = eta = 1/6
+mu = 1/2
 delta = 3
 alpha = 0.05
 rho = 1/6
 
-initVec = [N-100, 100, 0, 0, 0, 0] #Begin with N-1 susceptible, 1 exposed, 0 asymp/miserable, and no recovered or dead
+initVec = [N-1, 1, 0, 0, 0, 0] #Begin with N-1 susceptible, 1 exposed, 0 asymp/miserable, and no recovered or dead
 
 #Integrate the differential equations
-sol = odeint(SEIR, initVec, time, args = (N, beta, gamma1, gamma2, miu, delta, alpha, rho))
+sol = odeint(SEIR, initVec, time, args = (N, beta, gamma1, gamma2, eta, mu, delta, alpha, rho))
 S, E, A, M, R, D = sol.T
 
 #Plot the results
@@ -51,7 +52,7 @@ plt.subplots_adjust(left=0.25, bottom=0.25)
 Splt, = ax.plot(time, S, 'b', label = 'Susceptible')
 Eplt, = ax.plot(time, E, 'c', label = 'Exposed')
 Aplt, = ax.plot(time, A, 'r', label = 'Asymptotic')
-Mplt, = ax.plot(time, M, 'o', label = 'Miserable')
+Mplt, = ax.plot(time, M, 'y', label = 'Miserable')
 Rplt, = ax.plot(time, R, 'g', label = 'Recovered')
 Dplt, = ax.plot(time, D, 'k', label = 'Dead')
 Tplt, = ax.plot(time, S + E + A + M + R + D, '--', label = 'Total') #Mark total with a dashed line
@@ -64,8 +65,8 @@ sliderColor = 'green'
 betaAx = plt.axes([0.25, 0.15, 0.65, 0.03])
 betaSlider = Slider(betaAx, 'Beta (expected number of people infected per day)', 1, 10, valinit = 4, valstep = 1, color = sliderColor)
 
-miuAx = plt.axes([0.25, 0.10, 0.65, 0.03])
-miuSlider = Slider(miuAx, 'proportion of people getting miserable', 0, 1, valinit = 1/2, valstep = 1/10, color = sliderColor)
+muAx = plt.axes([0.25, 0.10, 0.65, 0.03])
+muSlider = Slider(muAx, 'proportion of people getting miserable', 0, 1, valinit = 1/2, valstep = 1/10, color = sliderColor)
 
 DAx = plt.axes([0.25, 0.20, 0.65, 0.03])
 DSlider = Slider(DAx, 'Days to transition (E->A orA->M) (1/Gamma) (1/Gamma)', 1, 15, valinit = 10, valstep = 1, color = sliderColor)
@@ -78,7 +79,7 @@ def reset(event):
 	betaSlider.reset()
 	DSlider.reset()
 	deltaSlider.reset()
-	miuSlider.reset()
+	muSlider.reset()
 
 resetAx = fig.add_axes([0.45, 0.025, 0.1, 0.04])
 resetButton = Button(resetAx, 'Reset', color = 'gray')
@@ -87,12 +88,12 @@ resetButton.on_clicked(reset)
 def model(event):
 	'''Update the parameters and plots'''
 	beta = betaSlider.val
-	gamma1 = gamma2 = 1/DSlider.val
+	gamma1 = gamma2 = eta = 1/DSlider.val
 	delta = deltaSlider.val
-	miu = miuSlider.val
+	mu = muSlider.val
 
 	#Differentiate the equations again
-	sol = odeint(SEIR, initVec, time, args = (N, beta, gamma1, gamma2, miu, delta, alpha, rho))
+	sol = odeint(SEIR, initVec, time, args = (N, beta, gamma1, gamma2, eta,mu, delta, alpha, rho))
 	S, E, A, M, R, D = sol.T
 
 	#Change the y-data on each of the plots
